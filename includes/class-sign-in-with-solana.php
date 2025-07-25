e6d4ff0a4ffd570fd4b074400091e11c90e94de0
t4top <temitope@yahoo.co.jp>
2025-07-25 09:26:28 +0000

    Enqueue compiled script files

--- includes/class-sign-in-with-solana.php
+++ includes/class-sign-in-with-solana.php
@@ -32,5 +32,32 @@ class Sign_In_With_Solana {
 	 * Register action hooks
 	 */
 	private function register_hooks() {
+		// enqueue style and javascript files
+		add_action( 'login_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+	}
+
+
+	/**
+	 * Register JS scripts and CSS styles
+	 */
+	public function enqueue_scripts() {
+		// enqueue js files
+		$js  = PLUGIN_URL . '/build/index.js';
+		$php = PLUGIN_DIR . '/build/index.asset.php';
+		$handle = PLUGIN_ID . '_js';
+
+		$dependency = require $php;
+		array_push( $dependency['dependencies'], 'jquery' );
+		wp_register_script( $handle, $js, $dependency['dependencies'], $dependency['version'], true );
+		wp_localize_script(
+			$handle,
+			'SignInWithSolana',
+			array(
+				'ajaxurl'  => admin_url( 'admin-ajax.php' ),
+				'pluginId' => PLUGIN_ID
+			)
+		);
+		wp_enqueue_script( $handle );
 	}
 }
