700a36cd622632e2094061412c2d8e64ca0d6be3
t4top <temitope@yahoo.co.jp>
2025-08-05 08:13:38 +0000

    Implement base58 decoding utilities using GMP and BCMath

--- includes/functions.php
+++ includes/functions.php
@@ -14,6 +14,36 @@ if ( ! defined( 'WPINC' ) ) {
 
 
 /**
+ * Check if BC Math Arbitrary Precision Mathematics extension is installed or not.
+ *
+ * @return bool true if service is installed, otherwise false.
+ */
+function is_bcmath_installed() {
+	return extension_loaded('bcmath');
+}
+
+
+/**
+ * Check if GNU Multiple Precision Mathematics extension is installed or not.
+ *
+ * @return bool true if service is installed, otherwise false.
+ */
+function is_gmp_installed() {
+	return extension_loaded('gmp');
+}
+
+
+/**
+ * Check if WooCommerce plugin is activated or not.
+ *
+ * @return bool true if WooCommerce is activated, otherwise false.
+ */
+function is_woocommerce_activated() {
+	return in_array( 'woocommerce/woocommerce.php', get_option( 'active_plugins', array() ) );
+}
+
+
+/**
  * Generate standardized hook name
  *
  * by prefixing with PLUGIN_ID, replacing dashes with underscores, and converting to lowercase.
@@ -31,16 +61,6 @@ function get_hook_name( $name ) {
 
 
 /**
- * Check if WooCommerce plugin is activated or not.
- *
- * @return bool true if WooCommerce is activated, otherwise false.
- */
-function is_woocommerce_activated() {
-	return in_array( 'woocommerce/woocommerce.php', get_option( 'active_plugins', array() ) );
-}
-
-
-/**
  * Shorten a given wallet address
  *
  * by returning the first 6 and last 6 characters, with "..." in between.
@@ -49,5 +69,93 @@ function is_woocommerce_activated() {
  * @return string The shortened address or an empty string if input is null or empty.
  */
 function shorten_address( $address ) {
-    return empty( $address ) ? '' : substr( $address, 0, 6 ) . '...' . substr( $address, -6 );
+	return empty( $address ) ? '' : substr( $address, 0, 6 ) . '...' . substr( $address, -6 );
+}
+
+
+/**
+ * Decode a Base58-encoded string to binary using GMP
+ *
+ * @param string $input Base58-encoded string
+ * @return string Binary decoded string
+ * @throws InvalidArgumentException if input contains invalid characters
+ */
+function base58_decode_gmp( $input ) {
+	$alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
+	$base = strlen( $alphabet );
+	$length = strlen( $input );
+	$decoded = gmp_init( 0, 10 );
+
+	for ( $i = 0; $i < $length; $i++ ) {
+		$char = $input[ $i ];
+		$pos = strpos( $alphabet, $char );
+
+		if ( $pos === false ) {
+			throw new InvalidArgumentException( "Invalid Base58 character: $char" );
+		}
+
+		$decoded = gmp_add( gmp_mul( $decoded, $base ), $pos );
+	}
+
+	$hex = gmp_strval( $decoded, 16 );
+	if ( strlen( $hex ) % 2 !== 0 ) {
+		$hex = '0' . $hex;
+	}
+
+	$binary = hex2bin( $hex );
+
+	// handle leading zeros (represented as "1" in base58)
+	$pad = 0;
+	while ( $pad < $length && $input[ $pad ] === '1' ) {
+		$pad++;
+	}
+
+	return str_repeat( "\x00", $pad ) . $binary;
+}
+
+
+/**
+ * Decode a Base58-encoded string to binary using BCMath
+ *
+ * @param string $input Base58-encoded string
+ * @return string Binary decoded string
+ * @throws InvalidArgumentException if input contains invalid characters
+ */
+function base58_decode_bcmath( $input ) {
+	$alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
+	$base = strlen( $alphabet );
+	$length = strlen( $input );
+	$num = '0';
+
+	// Convert Base58 to base10 using bcmath
+	for ( $i = 0; $i < $length; $i++ ) {
+		$char = $input[ $i ];
+		$pos = strpos( $alphabet, $char );
+
+		if ( $pos === false ) {
+			throw new InvalidArgumentException( "Invalid Base58 character: $char" );
+		}
+
+		$num = bcmul( $num, $base );
+		$num = bcadd( $num, (string) $pos );
+	}
+
+	// Convert base10 to hex
+	$hex = '';
+	while ( bccomp( $num, '0' ) > 0 ) {
+		$rem = bcmod( $num, '256' );
+		$num = bcdiv( $num, '256', 0 );
+		$hex = str_pad( dechex( $rem ), 2, '0', STR_PAD_LEFT ) . $hex;
+	}
+
+	// Convert hex to binary
+	$binary = $hex === '' ? '' : hex2bin( $hex );
+
+	// Handle leading zeros (encoded as '1's in Base58)
+	$pad = 0;
+	while ( $pad < $length && $input[ $pad ] === '1' ) {
+		$pad++;
+	}
+
+	return str_repeat( "\x00", $pad ) . $binary;
 }
